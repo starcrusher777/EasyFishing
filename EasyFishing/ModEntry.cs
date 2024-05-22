fa4a8175f2ac0d5ffcbc12bf58235a02794e0cbc
starcrusher777 <vasya.vasilev11@gmail.com>
2024-05-22 17:47:19 +0300

    Initial commit

---
+++ EasyFishing/ModEntry.cs
@@ -0,0 +1,32 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+
+namespace EasyFishing
+{
+    internal sealed class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.GameLoop.UpdateTicked += this.OnUpdateTicked;
+            Monitor.Log("Easy Fishing Mod loaded", LogLevel.Info);
+        }
+
+        private void OnUpdateTicked(object sender, UpdateTickedEventArgs e)
+        {
+            if (Game1.player.CurrentTool is StardewValley.Tools.FishingRod fishingRod)
+            {
+                if (fishingRod.isFishing && fishingRod.hit)
+                {
+                    if (fishingRod.timeUntilFishingBite > 0)
+                    {
+                        fishingRod.timeUntilFishingBite = 0;
+                    }
+                    fishingRod.castingPower = 1f;
+                    fishingRod.isReeling = true;
+                    fishingRod.fishCaught = true;
+                }
+            }
+        }
+    }
+}
